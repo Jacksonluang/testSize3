55f829537f9070734f97ead8ba0b5105558bb25b
梁杰 <515298945@qq.com>
2020-09-03 19:28:08 +0800

    update

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for i in 1
+for i in 1 2 3 4 5 6 7 8 9 10
   do
-    dd if=/dev/zero of=jeuddrnkt$i.data bs=4m count=10
+    dd if=/dev/zero of=jeuddrsxsxnkt$i.data bs=4m count=10
   done
 
